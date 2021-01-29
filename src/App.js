0364713090db8a4c01177b55117d5ac1183af984
Matt Potts <61755051+Cavematt@users.noreply.github.com>
2021-01-29 22:04:22 +0000

    Start it up!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar'
+import FrontPage from './components/FrontPage'
+import AboutMe from './components/AboutMe'
+import Business from './components/Business'
+import Portfolio from './components/Portfolio'
+import Security from './components/Security'
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <FrontPage />
+          </Route>
+          <Route path="/AboutMe" exact>
+            <AboutMe />
+          </Route>
+          <Route path="/Business" exact>
+            <Business />
+          </Route>
+          <Route path="/Portfolio" exact>
+            <Portfolio />
+          </Route>
+          <Route path="/Security" exact>
+            <Security />
+          </Route>
+          <Route path="/Contact" exact>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
