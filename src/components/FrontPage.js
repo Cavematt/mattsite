362243b7f95d5f8fcc7bbd54d5716ba0f09c2b56
Matt Potts <61755051+Cavematt@users.noreply.github.com>
2021-02-01 00:24:59 +0000

    frontpage update

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import "./FrontPage.css"
 import Me from "../images/me.jpg"
+import GitHub from "../images/GitHub.png"
+import LinkedIn from "../images/linked.png"
 
 function FrontPage() {
     const [line, setLine] = useState("")
@@ -25,9 +27,17 @@ function FrontPage() {
         <div className="fpAll">
             <div>
                 <div className="fpText">
-                    <p>Hello, thanks for taking the time to check out my site. Here you will find out a bit about me and how I can help your business by developing value custom solutions to help you concentrate on running your business. </p>
+                    <p>Hello, thanks for taking the time to check out my site. Here you will find out a bit about me and how I can help your business by developing value custom solutions to help you run your business more efficiently. </p>
                     <p>I specialise in React.js with a love for design and layout. I love to learn and am happy to learn new languages and skills if needed for a project. Please check out the rest of my site for further information</p>
                     <p className="fpTextSig">Matthew Potts</p>
+                    <div>
+                        <a href="https://github.com/Cavematt">
+                        <img src={GitHub} alt="Link to matt Potts Github" />
+                        </a>
+                        <a href="https://uk.linkedin.com/public-profile/in/matthew-potts-15b908196?">
+                        <img className="linkedIn"src={LinkedIn} alt="Link to matt Potts Linkedin" />
+                        </a>
+                    </div>
                 </div>
                 <div className="fpPhotoCont">
                     <img className="fpPhoto" src={Me} alt="display of me" />
