d4bb536ca8fd2ccac9186b162f57c68b141ff300
Matt Potts <61755051+Cavematt@users.noreply.github.com>
2021-02-18 23:12:45 +0000

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,23 @@ import "./Portfolio.css"
 function Portfolio() {
     return (
         <div className="portAll">
-            portfolio
+            <div>
+                <div>
+                    <h1>Portfolio</h1>
+                </div>
+                <div>
+                    <p>Below is a small selection of my work to demonstrate my abilities as well as some of my customers.</p>
+                </div>
+                <div>
+                    <p>This is a website for a hairdresser informing customers about her work as well as other information such as experience and contact details. It is fully animated and compatible with desktop, tablet and mobile devices.</p>
+                </div>
+                <div>
+                    <p>This is for a local haulage company. They wanted a low maintenance simple website with basic information about their company. It is compatible with desktop and mobile. I also made an accompanying mobile application for their driver to access health and safety information as well as other info such as newsletters. I am also building an e-induction platform for their drivers to receive training.</p>
+                </div>
+                <div>
+                    <p>This is an example online shop built using React and users can create accounts and purchase products fully online. It is customisable to your business if required or I can build a custom platform.</p>
+                </div>
+            </div>
         </div>
     )
 }
