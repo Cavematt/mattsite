10052743128f87122641dc9d907ddc331703e6f2
Matt Potts <61755051+Cavematt@users.noreply.github.com>
2021-02-18 22:25:32 +0000

    Update Business.js

--- src/components/Business.js
+++ src/components/Business.js
@@ -3,7 +3,26 @@ import React from 'react'
 function Business() {
     return (
         <div>
-            Busines
+            <div>
+                <div>
+                    <p>I want to make your website fit you and your business. As I design each website “from scratch” I can make something truly unique and minimise any ongoing costs which can be as little as £5 a month including a domain which can be significantly cheaper than many drag and drop web builders where your site is built from a template. I can integrate most features as required and can be anything from an online shop or a single page application to an online portal where you login to view documents or training for your employees. </p>
+                </div>
+                <div>
+                    <p>After an initial enquiry I will arrange for a free initial consultation to take place where we decide on what design, features, technical requirements and timescales are required and any estimated costs for the project. I can assist in writing content and choosing graphics if required.</p>
+                </div>
+                <div>
+                    <p>I will then get building and make your site to the required design. I will send you a web address where you can view your website as it is being made so you can check on the progress. If there is anything you want to amend then feel free to contact me at any time</p>
+                </div>
+                <div>
+                    <p>Once built and you confirm you are happy then I will link it to your chosen domain, assist with listing on major search engines and agree any ongoing maintenance needed or any improvements you wish to make in the future.</p>
+                </div>
+                <div>
+                    <p>I can host your product using Amazon Web Services Or Microsoft Azure to your preference. </p>
+                </div>
+                <div>
+                    <p>Please click on portfolio on the menu to see some of my work.</p>
+                </div>
+            </div>
         </div>
     )
 }
